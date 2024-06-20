7ccfa29170419d654c2de4ea5d4f2d522c9c063c
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-06-20 09:00:45 -0400

    Create phylophlan.sh

---
+++ Slurm batch scripts/phylophlan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=agrp
+#SBATCH --time=14-00:00:00
+#SBATCH -N 1 # Nodes
+#SBATCH -n 1 # Tasks
+#SBATCH --mem 100G
+#SBATCH --cpus-per-task=36
+#SBATCH --error=logs/phylophlan.%J.err
+#SBATCH --output=logs/phylophlan.%J.out
+
+cd /path/to/phylophlan/input/files
+
+eval "$(conda shell.bash hook)"
+conda activate phylophlan
+
+phylophlan -i Input_folder -o Phylophlan_output -d phylophlan --diversity low -f supermatrix_aa.cfg --nproc 36
+
+conda deactivate
