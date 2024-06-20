642a7f2d2c6007f63df5233ca93c2cc0ca358272
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-06-20 08:58:17 -0400

    Create prodigal.sh

---
+++ Slurm batch scripts/prodigal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --partition=agrp
+#SBATCH --time=14-00:00:00
+#SBATCH -N 1 # Nodes
+#SBATCH -n 1 # Tasks
+#SBATCH --cpus-per-task=10
+#SBATCH --mem 10G
+#SBATCH --error=prokka.%J.err
+#SBATCH --output=prokka.%J.out
+#SBATCH --mail-user=your_email@gmail.com
+#SBATCH --mail-type=ALL
+
+cd /path/to/your/MAGs
+
+source /home/sam/miniconda3/bin/activate prodigal
+
+for genome_bin in `ls *.fasta`
+do
+        prodigal -i ${genome_bin} -a ${genome_bin/.fasta/.faa} -f gff -p meta -o ${genome_bin/.fasta/.gff}
+done
