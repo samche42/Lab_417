304d455108ca7ba2e9781b7b75d7afcea856e1ba
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-06-20 08:54:59 -0400

    Create spades_illumina.sh

---
+++ Slurm batch scripts/spades_illumina.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=agrp
+#SBATCH --time=14-00:00:00
+#SBATCH -N 1 # Nodes
+#SBATCH -n 1 # Tasks
+#SBATCH --cpus-per-task=24
+#SBATCH --mem 500G
+#SBATCH --error=logs/spades.%J.err
+#SBATCH --output=logs/spades.%J.out
+
+cd /path/to/reads
+
+python3 /home/USER_HERE/Tools/SPAdes-3.15.0-Linux/bin/spades.py -t 24 -m 500 \
+        --pe1-1 fwd_reads.fastq --pe1-2 rev_reads.fastq \
+        -o RENAME_ME_Spades_Assembly
+
+#NOTE: CHANGE PATH TO SPADES TO WHEREVER YOU DOWNLOADED YOURS
