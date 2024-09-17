906433055bd6e27c5bd385f783d4efdf7d8e58d6
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-09-17 14:34:25 -0400

    Update blastp.sh

--- Slurm batch scripts/blastp.sh
+++ Slurm batch scripts/blastp.sh
@@ -19,7 +19,7 @@ for file in `ls *.faa`
 
 do
 
-diamond blastp -d /home/sam/Databases/nr.dmnd -q ${file} -\
+diamond blastp -d /home/sam/Databases/autometa_ncbi_sam/nr.dmnd -q ${file} -\
 k 1 --max-hsps 1 --outfmt 6 qseqid stitle pident evalue qlen slen -o ${file/.faa/_blast.tab}
 
 done
